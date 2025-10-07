db2b63cbb874567e0faad998c6dc47a2f3407014
emmuel225 <emmuel225@yahoo.com>
2025-10-07 12:01:55 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function addthreenumbers () {
+    var num1 = parseInt9( prompt("ENTER THE FIRST MUMBER:"));
+    var num2 = parseInt9( prompt("ENTER THE FIRST MUMBER:"));
+    var num3 = parseInt9( prompt("ENTER THE FIRST MUMBER:"));
+    Var sum = num1 + num2 + num3;
+    alert(sum);
+}
